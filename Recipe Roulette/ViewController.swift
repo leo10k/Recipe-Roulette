b0f02b3ddbe4ef37374216fbd13f9b118b9219c6
leo10k <leozinhomacedo41@gmail.com>
2024-01-24 07:03:51 -0300

    deleting storyboard stuffs

---
+++ Recipe Roulette/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Recipe Roulette
+//
+//  Created by Leonardo Macedo on 24/01/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .cyan
+    }
+
+
+}
+
